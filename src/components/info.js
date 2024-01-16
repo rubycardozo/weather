d5239036b2f4a1835c960ad083c70bdb77f1d776
Zingo66 <rubyjunecar@gmail.com>
2024-01-16 16:14:29 -0500

    commit

--- src/components/info.js
+++ src/components/info.js
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from "react";
-import Misty from './images/701.jpg'
+import Snow from './images/snow.png'
+import Clear from './images/clear.png'
+import Drizzle from './images/drizzle.png'
+import Rain from './images/rain.png'
+import Clouds from './images/clouds.png'
+import Thunderstorm from './images/thunderstorm.png'
 
 const Info = ({ weatherData }) => {
   const [condition, setCondition] = useState("Unknown");
@@ -7,6 +12,7 @@ const Info = ({ weatherData }) => {
   useEffect(() => {
     const getId = (weatherId) => {
       if (weatherId >= 200 && weatherId < 300) {
+
         return "Thunderstorm";
       } else if (weatherId >= 300 && weatherId < 500) {
         return "Drizzle";
@@ -26,10 +32,34 @@ const Info = ({ weatherData }) => {
         return "Unknown";
       }
     };
+    const getIcon = (icon) => {
+      if (weatherId >= 200 && weatherId < 300) {
+
+        return Thunderstorm;
+      } else if (weatherId >= 300 && weatherId < 500) {
+        return Drizzle;
+      } else if (weatherId >= 500 && weatherId < 600) {
+        return Rain;
+      } else if (weatherId >= 600 && weatherId < 700) {
+        return Snow;
+      } else if (weatherId === 800) {
+        return Clear;
+      } else if (weatherId === 701) {
+        return Misty;
+      } 
+      else if (weatherId > 800) {
+        return Clouds;
+       
+      } else {
+        return "Unknown";
+      }
+    };
     
       const weatherId = weatherData.weather[0].id;
       const currentCondition = getId(weatherId);
+      const currentIcon = getIcon(icon)
       setCondition(currentCondition);
+      setIcon(currentIcon)
 
 
 
@@ -38,7 +68,7 @@ const Info = ({ weatherData }) => {
 
   return (
     <div className='weathercard'>
-      <div  src={icon}></div>
+      <img className="icon" src={icon}/>
       <h2>{weatherData.main?.temp} &deg;</h2>
       <p>{condition}</p>
       <p>Wind Speed: {weatherData.wind?.speed} mph</p>
